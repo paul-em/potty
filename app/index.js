e85631c4bdb745bc15e1b8c2d38798bcbfc81194
Paul Em <paul3m@gmail.com>
2016-11-26 13:58:42 +0100

    removed wireless component - moved to system level

--- app/index.js
+++ app/index.js
@@ -1,16 +1,11 @@
-
-
-const wireless = require('./wireless');
 const nas = require('./nas');
 const hue = require('./hue');
 const playsound = require('./playsound');
 
 playsound.playBootSong();
 
-wireless.connect().then(() => {
-    nas.connect()
-        .then(()=>nas.loadSounds())
-        .then((sounds)=>playsound.play(sounds));
+nas.connect()
+  .then(() => nas.loadSounds())
+  .then((sounds) => playsound.play(sounds));
 
-    hue.connect(()=>hue.pulsate());
-});
+hue.connect(() => hue.pulsate());
